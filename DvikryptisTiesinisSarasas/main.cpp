11c77109da2f58fb46814313a1dfaf2e9e846b3c
edmundasx <48953028+edmundasx@users.noreply.github.com>
2019-12-03 03:36:16 +0200

    Add files via upload

--- DvikryptisTiesinisSarasas/main.cpp
+++ DvikryptisTiesinisSarasas/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 struct sarasas
 {
@@ -18,10 +19,12 @@ class dvikryptis
      sarasas *GetPabaiga(){return pabaiga;};
      void Ikelti(FILE*);
      void Spausdinti();
-     void Tikrinti();
+     void Tikrinti(sarasas *);
      void Prideti(sarasas *, int *);
      int Salinti(int);
      int Ieskoti(int);
+     void menu();
+     void text();
 };
 dvikryptis::~dvikryptis()
 {
@@ -33,25 +36,27 @@ dvikryptis::~dvikryptis()
       delete(kint->elementas);
       delete(kint);
     }
-
 };
 int main(){
+ string failopav;
+ char skaicius;
+ int elementas;
+
+cout<<"Iveskite failo pavadinima"<<endl;
+cin>>failopav;
 FILE *d;
 dvikryptis objektas;
-d=fopen("skaiciai.txt", "r");
+d=fopen(failopav.c_str(), "r");
 if(d==NULL)
 {
     cout<<"Failas Nerastas"<<endl;
     exit(1);
 }
 objektas.Ikelti(d);
-objektas.Spausdinti();
-int a=3;
-objektas.Spausdinti();
-objektas.Salinti(a);
+objektas.menu();
 
-objektas.Spausdinti();
-objektas.Ieskoti(45);
+//objektas.Ikelti(d);
+
 }
 void dvikryptis::Ikelti(FILE *failas)
 {
@@ -94,13 +99,12 @@ void dvikryptis::Prideti(sarasas *kint, int *sk)
         kint2->kairys=kint->kairys;
         (kint->kairys)->desinys=kint2;
         kint->kairys=kint2;
-
     }
 }
 int dvikryptis::Salinti(int sk)
 {
-int pos(0);
-        sarasas *kint=pabaiga;
+
+    sarasas *kint=pabaiga;
     while(kint!=NULL)
     {
     if(*kint->elementas==sk)
@@ -123,9 +127,7 @@ int dvikryptis::Ieskoti(int sk)
         {
         flag=1;
         pos--;
-
         break;
-
     }
     kint=kint->kairys;
       }
@@ -139,6 +141,109 @@ int dvikryptis::Ieskoti(int sk)
         cout<<"Elementas nerastas."<<endl;
         }
 }
+void dvikryptis::Tikrinti(sarasas*)
+{
+sarasas *kint=pabaiga;
+if(kint->kairys==NULL&&kint->desinys==NULL)
+{
+    cout<<"Sarasas tuscias"<<endl;
+}
+else
+{
+    cout<<"Sarasas netuscias"<<endl;
+}
+}
+void dvikryptis::menu()
+{
+    int elementas;
+    char skaicius;
+    char select;
+    system("CLS");
+    text();
+
+
+    cin>>skaicius;
+    switch(skaicius)
+{
+case '1':
+    Spausdinti();
+    cout<<"Testi darba?[T/N]"<<endl;
+    cin>>select;
+    if(select=='t')
+    {
+            text();
+            menu();
+    }
+    else{exit;}
+    break;
+case '2':
+    system("CLS");
+    cout<<"Iveskite elementa kuri norite pasalinti:";
+    cin>>elementas;
+    Salinti(elementas);
+    cout<<"Testi darba?[T/N]"<<endl;
+    cin>>select;
+    if(select=='t')
+    {
+            text();
+            menu();
+    }
+    else{exit;}
+    break;
+case '3':
+    system("CLS");
+    cout<<"Iveskite elementa kuri norite prideti:";
+    cin>>elementas;
+    Salinti(elementas);
+    cout<<"Testi darba?[T/N]"<<endl;
+    cin>>select;
+    if(select=='t')
+    {
+            text();
+            menu();
+    }
+    else{exit;}
+    break;
+case '4':
+    system("CLS");
+    cout<<"Iveskite elementa kuri norite surasti:";
+    cin>>elementas;
+    Ieskoti(elementas);
+    cout<<"Testi darba?[T/N]"<<endl;
+    cin>>select;
+    if(select=='t')
+    {
+            text();
+            menu();
+    }
+    else{exit;}
+    break;
+case '5':
+    system("CLS");
+    Tikrinti(GetPabaiga());
+    cout<<"Testi darba?[T/N]"<<endl;
+    cin>>select;
+    if(select=='t')
+    {
+            text();
+            menu();
+    }
+    else{exit;}
+    break;
+    break;
+}
+}
+void dvikryptis::text()
+{
+
+cout<<"Pasirinkite:"<<endl;
+cout<<"1):Spausdinti"<<endl;
+cout<<"2):Salinti"<<endl;
+cout<<"3):Prideti"<<endl;
+cout<<"4):Ieskoti"<<endl;
+cout<<"5):Tikrinti ar tuscias"<<endl;
+
+}
 void dvikryptis::Spausdinti()
 {
     sarasas *kint=pabaiga;
@@ -149,5 +254,3 @@ void dvikryptis::Spausdinti()
     }
     cout<<endl;
 }
-
-
